de6e3828cd407faa3227381a323b4e71c4b58c15
Danichp <danichp01@mail.ru>
2024-04-22 12:58:32 +0700

    added overview and fix hero and added format script

--- eslint.config.js
+++ eslint.config.js
@@ -3,12 +3,12 @@ import pluginJs from '@eslint/js';
 import { rules } from 'eslint-config-prettier';
 
 export default [
-    { languageOptions: { globals: globals.browser } },
-    pluginJs.configs.recommended,
-    {
-        extends: ['eslint:recommended', 'plugin:prettier/recomended'],
-        rules: {
-            'prettier/prettier': 'error',
-        },
+  { languageOptions: { globals: globals.browser } },
+  pluginJs.configs.recommended,
+  {
+    extends: ['eslint:recommended', 'plugin:prettier/recomended'],
+    rules: {
+      'prettier/prettier': 'error',
     },
+  },
 ];

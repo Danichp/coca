11e1a1c5e17c75c4cca5837155d1ca06dcc9e1d3
Danichp <danichp01@mail.ru>
2024-04-25 16:53:29 +0700

    added pricing:section hero

---
+++ src/js/components/planSwitcher.js
@@ -0,0 +1,30 @@
+export const usePlanSwitcher = () => {
+  const priceArray = document.querySelectorAll('.hero__plan-price-num');
+  const priceTimeArray = document.querySelectorAll('.hero__plan-price-text');
+  const switcher = document.querySelector('.hero__switcher-input-checkbox');
+  const priceNumArray = [];
+
+  priceArray.forEach((el) => {
+    priceNumArray.push(+el.innerHTML);
+  });
+
+  switcher.checked = true;
+
+  switcher.addEventListener('change', () => {
+    if (switcher.checked) {
+      for (let i = 0; i < priceArray.length; i++) {
+        priceArray[i].textContent = priceNumArray[i];
+      }
+      priceTimeArray.forEach((el) => {
+        el.innerHTML = '/mo';
+      });
+    } else {
+      for (let i = 0; i < priceArray.length; i++) {
+        priceArray[i].textContent = (priceNumArray[i] * 1.2) / 30;
+      }
+      priceTimeArray.forEach((el) => {
+        el.innerHTML = '/day';
+      });
+    }
+  });
+};
---
+++ src/js/pricing.js
@@ -0,0 +1,12 @@
+import '../assets/scss/pricing.scss';
+
+// components
+import { useTheme } from './components/theme';
+import { useBurger } from './components/burger';
+import { useAccordion } from './components/accordion';
+import { usePlanSwitcher } from './components/planSwitcher';
+
+useTheme();
+useBurger();
+useAccordion();
+usePlanSwitcher();

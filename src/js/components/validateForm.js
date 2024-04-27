b37fa82db0ac5e2f46fc3a075edf1fb90bedca3e
Danichp <danichp01@mail.ru>
2024-04-27 15:46:18 +0700

    added contact:get-started

---
+++ src/js/components/validateForm.js
@@ -0,0 +1,49 @@
+export const useValidateForm = () => {
+  const form = document.querySelector('.get-started__form'); //form submit
+
+  form.addEventListener('submit', formSend);
+
+  async function formSend(e) {
+    e.preventDefault(); //запрещаем стандартную отправку формы
+    let error = formValidate(form);
+
+    let formData = new FormData(form);
+    if (error === 0) {
+      alert('Форма отправлена');
+      form.reset();
+    }
+  }
+
+  function formValidate(e) {
+    let error = 0;
+    let formReq = document.querySelectorAll('._req');
+
+    for (let i = 0; i < formReq.length; i++) {
+      const input = formReq[i];
+      formRemoveError(input);
+
+      if (input.classList.contains('tel')) {
+        if (!telTest(input)) {
+          formAddError(input);
+          error++;
+        }
+      } else if (input.classList.contains('_name')) {
+        if (input.value == '') {
+          formAddError(input);
+          error++;
+        }
+      }
+    }
+    return error;
+  }
+
+  function formAddError(input) {
+    input.classList.add('error');
+  }
+  function formRemoveError(input) {
+    input.classList.remove('error');
+  }
+  function telTest(input) {
+    return input.value.lenght !== 17;
+  }
+};
--- src/js/contact.js
+++ src/js/contact.js
@@ -6,9 +6,13 @@ import { useBurger } from './components/burger';
 import { useAccordion } from './components/accordion';
 import { useMapYandex } from './components/mapYandex';
 import { usePartnersSlider } from './components/slider';
+import { useValidateForm } from './components/validateForm';
+import { useMaskTel } from './components/maskTel';
 
 useTheme();
 useBurger();
 useAccordion();
 usePartnersSlider();
 useMapYandex();
+useValidateForm();
+useMaskTel();
